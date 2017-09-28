f6ce859ca2dc494bdc8a53a18264a0b807e3cc3f
Miles Macchiaroli <macchiarolim@wit.edu>
2017-09-27 22:40:40 -0400

    Created, tested, and commented PA3a and PA3b

--- src/edu/wit/cs/comp1000/PA3a.java
+++ src/edu/wit/cs/comp1000/PA3a.java
@@ -1,11 +1,56 @@
 package edu.wit.cs.comp1000;
 
-//TODO: document this class
-public class PA3a {
+/**
+ * PA3a
+ * @author Miles Macchiaroli
+ *
+ */
+import java.util.Scanner; //Link Scanner Utility
+public class PA3a {//Begin Class
 
-	// TODO: document this method
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+	/**
+	 * This Program Calculates the Area of a Triangle
+	 * Utilizing Heron's Formula
+	 * @param args
+	 */
+	public static void main(String[] args) {//Begin Main String execution
+		Scanner input = new Scanner(System.in); //Call for Scanner Utility
+	//INIT VARS
+		double a, b, c, s, area;
+	//Begin Request
+		System.out.printf("Enter the length of side a: ");
+		a = input.nextDouble(); // request value a
+		System.out.printf("Enter the length of side b: ");
+		b = input.nextDouble(); // request value b
+		System.out.printf("Enter the length of side c: ");
+		c = input.nextDouble(); // request value c
+	//End Request
+	//Begin Value Check
+		if ((a < 0) || (b < 0) || (c < 0)) {//Check all inputs positive
+			System.out.printf("Side lengths must all be positive%n");
+			System.exit(0);
+		}
+		else if (c + b < a){//check if a is less than c+b
+			System.out.printf("Side a is too long%n");
+			System.exit(0);
+		}
+		else if (a + c < b){//check if b is less than c+a
+			System.out.printf("Side b is too long%n");
+			System.exit(0);
+		}
+		else if (a + b < c){//check if c is less than a+b
+			System.out.printf("Side c is too long%n");
+			System.exit(0);
+		}
+		else {
+	//End Value Check
+	//Begin Calculations
+		s = ((a + b + c)/2.0);//Calculate semiperimeter
+		area = Math.sqrt( s * (s - a) * (s - b) * (s - c));//Calculate Area
+	//End Calculations
+	//Begin Output
+		System.out.printf("The area is %.2f%n", area);
+	//End Output
+		}}
 
 }
--- src/edu/wit/cs/comp1000/PA3b.java
+++ src/edu/wit/cs/comp1000/PA3b.java
@@ -1,11 +1,140 @@
 package edu.wit.cs.comp1000;
 
-//TODO: document this class
-public class PA3b {
-
-	// TODO: document this method
-	public static void main(String[] args) {
-		// TODO: write your code here
+/**
+ * PA3b
+ * @author Miles Macchiaroli
+ *
+ */
+import java.util.Scanner; //Link Scanner Utility
+public class PA3b {//Begin Class
+/**
+ * This Program Calculates the Astrological Sign
+* Based on the user's birthday
+* @param args
+*/
+	public static void main(String[] args) {//Begin Main String execution
+		Scanner input = new Scanner(System.in); //Call for Scanner Utility
+	//INIT VARS
+		int month, day;
+		String sign="sign";
+	//Begin Request
+		System.out.printf("Enter your birth month (1-12): ");
+		month = input.nextInt(); // request month
+		System.out.printf("Enter your birth day (1-31): ");
+		day = input.nextInt(); // request day
+	//End Request
+	//Begin Input Check
+		if ((month < 1) || (month > 12)){
+		System.out.printf("Month must be between 1 and 12!%n");
+		System.exit(0);
 	}
-
-}
+		else if ((day < 1) || (day > 31)){
+			System.out.printf("Day must be between 1 and 31!%n");
+			System.exit(0);
+		}
+	//End Input Check
+	//Begin tedious sign declaration
+	
+	//MONTHS
+		if (month == 1) {//JANUARY
+			if (day < 20) {
+				sign=" Capricorn";
+			}
+			else {
+				sign="n Aquarius";
+			}
+		}
+		if (month == 2) {//FEB
+			if (day < 19) {
+				sign="n Aquarius";
+			}
+			else {
+				sign=" Pisces";
+			}	
+		}
+		if (month == 3) {//MAR
+			if (day < 21) {
+				sign=" Pisces";
+			}
+			else {
+				sign="n Aries";
+			}	
+		}
+		if (month == 4) {//APRIL
+			if (day < 20) {
+				sign="n Aries";
+			}
+			else {
+				sign=" Taurus";
+			}	
+		}
+		if (month == 5) {//MAY
+			if (day < 21) {
+				sign=" Taurus";
+			}
+			else {
+				sign=" Gemini";
+			}	
+		}
+		if (month == 6) {//JUNE
+			if (day < 22) {
+				sign=" Gemini";
+			}
+			else {
+				sign=" Cancer";
+			}	
+		}
+		if (month == 7) {//JULY
+			if (day < 23) {
+				sign=" Cancer";
+			}
+			else {
+				sign=" Leo";
+			}	
+		}
+		if (month == 8) {//AUG
+			if (day < 23) {
+				sign=" Leo";
+			}
+			else {
+				sign=" Virgo";
+			}	
+		}
+		if (month == 9) {//SEP
+			if (day < 23) {
+				sign=" Virgo";
+			}
+			else {
+				sign=" Libra";
+			}	
+		}
+		if (month == 10) {//OCT
+			if (day < 23) {
+				sign=" Libra";
+			}
+			else {
+				sign=" Scorpio";
+			}	
+		}
+		if (month == 11) {//NOV
+			if (day < 22) {
+				sign=" Scorpio";
+			}
+			else {
+				sign=" Sagittarius";
+			}	
+		}
+		if (month == 12) {//DEC
+			if (day < 22) {
+				sign=" Sagittarius";
+			}
+			else {
+				sign=" Capricorn";
+			}	
+		}
+//END tedious sign declaration
+	//OUTPUT
+		System.out.printf("You are a%s!%n", sign);
+		System.out.printf("[Insert super cool (totally original) horoscope for the user that is a%s]%n", sign);
+		
+}}
